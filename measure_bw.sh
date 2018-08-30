740e2cb9b0a154c4f4e590fed4b54f9a0f6c6a43
Ubuntu <oliver-dot-stein@hotmail.com>
2018-08-30 14:45:00 +0000

    added bw measurement tool

---
+++ measure_bw.sh
@@ -0,0 +1,12 @@
+echo 'time,out (MBit/s),in (kBit/s)' >> network_bw.csv
+i=0;
+txold="$(</sys/class/net/ens3/statistics/tx_bytes)"; 
+rxold="$(</sys/class/net/ens3/statistics/rx_bytes)"; 
+while $(sleep 1); do
+rxnow=$(</sys/class/net/ens3/statistics/rx_bytes); 
+txnow=$(</sys/class/net/ens3/statistics/tx_bytes); 
+echo $i','$((($txnow-$txold)*8/(1024*1024)))','$((($rxnow-$rxold)*8/(1024))) >> network_bw.csv; 
+txold=$txnow; 
+rxold=$rxnow;
+i=$(($i+1));
+done
