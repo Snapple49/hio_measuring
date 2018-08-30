64689b7840477bb77e5b7c6998617f8711ed2573
Oliver Stein <oliver-dot-stein@hotmail.com>
2018-08-30 15:22:19 +0000

    changed to kBit from MBit

--- measure_bw.sh
+++ measure_bw.sh
@@ -1,4 +1,4 @@
-echo 'time,out (kBit/s),in (MBit/s)' >> network_bw.csv
+echo 'time,out (kBit/s),in (kBit/s)' >> network_bw.csv
 i=0;
 txold="$(</sys/class/net/ens3/statistics/tx_bytes)"; 
 rxold="$(</sys/class/net/ens3/statistics/rx_bytes)"; 
@@ -7,7 +7,7 @@ while $(sleep 1); do
 rxnow=$(</sys/class/net/ens3/statistics/rx_bytes); 
 txnow=$(</sys/class/net/ens3/statistics/tx_bytes); 
 dknow=$(</sys/class/net/docker0/statistics/rx_bytes); 
-echo $i','$((($txnow-$txold-($dknow-$dkold))*8/(1024)))','$((($rxnow-$rxold)*8/(1024*1024))) >> network_bw.csv; 
+echo $i','$((($txnow-$txold-($dknow-$dkold))*8/(1024)))','$((($rxnow-$rxold)*8/(1024))) >> network_bw.csv; 
 txold=$txnow; 
 rxold=$rxnow; 
 dkold=$dknow; 
